1f81d1fb27bd420ad6a578e496f88c11cc36c41f
Whips <whips@MyComputer.noname.noorg>
2019-09-28 11:40:27 +0300

    api/task2 optimization and error check

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 
+// localhost:3000/api/about
 app.use("/api/about", function(request, response){
     response.send("<h1>Шимон Роберт Тамашович</h1><h2>СА2</h2><h2>Варінт 9</h2><h3>27.09.2019</h3>");
 });
 
+// localhost:3000/api/task1?n=123
 app.use("/api/task1", function(req, res){
     res.send(`${req.query.n ? req.query.n.length : "<b>Error.</b> Enter: localhost:3000/api/task1?n=[number]"}`);
 });
 
+// localhost:3000/api/task2?gender=Man
 app.use('/api/task2', function(req, res){
     if (!req.query.gender)
         res.send("<b>Error.</b> Enter: localhost:3000/api/task2?gender=[Man|Woman]");
@@ -24,10 +27,9 @@ app.use('/api/task2', function(req, res){
         {name:'Abbey S.', gender:'Woman', bdate:new Date(1995,1,0,0,0,0,0), education:'High', spec:'Statistics', accounting:undefined}
     ];
 
-    let currentYear = new Date().getFullYear();
-    let retirementAge = gender == 'Man' ? 65 : 55;
-    res.send(people.filter(x => x.gender == gender &&
-            currentYear - x.bdate.getFullYear() >= retirementAge));
+    // retirement
+    let maxBirthYear = new Date().getFullYear() - (gender == 'Man' ? 65 : 55);
+    res.send(people.filter(x => x.gender == gender && x.bdate.getFullYear() <= maxBirthYear));
 });
 
 app.listen(3000);
